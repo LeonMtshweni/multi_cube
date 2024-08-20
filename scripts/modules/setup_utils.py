c3647cb575988f5a4f31f3c076158d85677e085d
Leon <mac_leon@macbook.lan>
2024-08-20 18:37:20 +0200

    add config file

--- scripts/modules/setup_utils.py
+++ scripts/modules/setup_utils.py
@@ -16,7 +16,7 @@ def setup_project_structure():
     Set up the project structure by creating necessary directories in the parent directory.
     """
     # Define directories to create
-    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
     directories = [
         os.path.join(parent_dir, 'msdir'),
         os.path.join(parent_dir, 'outputs'),
