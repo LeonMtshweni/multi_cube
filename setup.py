90eac0b4f388080d75ef9b626e1563a53ce7e0f3
LeonMtshweni <u13371062@tuks.co.za>
2024-10-19 11:01:42 +0200

    fixed the long_description_content_type field in setup script

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name="multi-cube",  # Your package name on PyPI
     version="0.1",  # Initial version
     packages=find_packages(),  # Automatically find the packages
-    description="A convenience tool that orchestrates the parallel generation of FITS cubes from a continuum subtracted ms file.",
+    description="A convenience tool that orchestrates the parallel generation of FITS cubes from a continuum-subtracted ms file.",
     long_description=open("README.md").read(),  # Long description from your README
-    long_description_content_type="A convenience tool that orchestrates the parallel generation of FITS cubes from a continuum subtracted ms file. User modifies the configuration file multi_cube/config/config.yml",  # The README format
+    long_description_content_type="text/markdown",  # Specify the correct content type for the README
     author="Leon K.B. Mtshweni",
     author_email="leonkb.m.astro@gmail.com",
-    url="https://github.com/yourusername/multi-cube",
+    url="https://github.com/yourusername/multi-cube",  # Update this URL to your actual GitHub repository
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # Adjust if using another license
@@ -19,7 +19,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'multi_cube=scripts.makecube:main'
+            'multi_cube=scripts.makecube:main'  # This maps the command 'multi_cube' to the 'main' function in 'scripts.makecube'
         ],
     },
 )
